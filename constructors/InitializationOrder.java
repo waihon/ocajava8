4e871f4a71ef78828224270b72567c9e3760805d
Waihon Yew <yewwaihon@gmail.com>
2025-01-01 22:44:03 +0800

    Java constructors

---
+++ constructors/InitializationOrder.java
@@ -0,0 +1,32 @@
+package constructors;
+
+public class InitializationOrder {
+
+  private String name = "Torchie";
+
+  {
+    System.out.println(name);
+  }
+
+  private static int COUNT = 0;
+
+  static {
+    System.out.println(COUNT);
+  }
+
+  {
+    COUNT++;
+    System.out.println(COUNT);
+  }
+
+  public InitializationOrder() {
+    System.out.println("constructor");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("read to construct");
+    new InitializationOrder();
+    System.out.println("write to construct");
+  }
+
+}
---
+++ constructors/YetMoreInitializationOrder.java
@@ -0,0 +1,35 @@
+package constructors;
+
+public class YetMoreInitializationOrder {
+  static {
+    add(2);
+  }
+
+  static void add(int num) {
+    System.out.print(num + " ");
+  }
+
+  YetMoreInitializationOrder() {
+    add(5);
+  }
+
+  static {
+    add(4);
+  }
+
+  {
+    add(6);
+  }
+
+  static {
+    new YetMoreInitializationOrder();
+  }
+
+  {
+    add(8);
+  }
+
+  public static void main(String[] args) {
+    add(10);
+  }
+}
