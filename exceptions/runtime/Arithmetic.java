f5a2d9f28505af7c4b013cc2137b9df27cd94e81
Waihon Yew <yewwaihon@gmail.com>
2025-02-06 20:13:21 +0800

    Runtime exceptions: ArithmeticException

---
+++ exceptions/runtime/Arithmetic.java
@@ -0,0 +1,9 @@
+package exceptions.runtime;
+
+public class Arithmetic {
+  public static void main(String[] args) {
+    // ArithmeticException: / by zero
+    int answer = 18 / 0;
+    System.out.println(answer);
+  }
+}
